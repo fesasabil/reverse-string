d59dc0e0a248aaccc68e9f330ce592ebd25f48f7
fesasabil <kijee212@gmail.com>
2018-10-09 10:58:39 +0700

    balikkata.html

---
+++ balikkata.js
@@ -0,0 +1,9 @@
+function balikkata(kata) {
+ return kata.split("").reverse("").join("");
+}
+
+console.log(balikkata('Hello Word and coders'));
+console.log(balikkata('John Doe'));
+console.log(balikkata('I am a bookworm'));
+console.log(balikkata('Coding is my hobby'));
+console.log(balikkata('Super'));
